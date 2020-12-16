8156f9e8584659ea028b56fea8042020572b47ee
Yuraul <yuraulio@gmail.com>
2020-12-16 14:57:44 +0200

    Add ajax callback and div element for the error messages in add FeedbackEntityAddForm.

--- src/Form/FeedbackEntityAddForm.php
+++ src/Form/FeedbackEntityAddForm.php
@@ -2,8 +2,12 @@
 
 namespace Drupal\yuraul\Form;
 
+use Drupal\Core\Ajax\AjaxResponse;
+use Drupal\Core\Ajax\HtmlCommand;
+use Drupal\Core\Ajax\RedirectCommand;
 use Drupal\Core\Entity\ContentEntityForm;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Url;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
@@ -37,6 +41,18 @@ class FeedbackEntityAddForm extends ContentEntityForm {
     /* @var \Drupal\yuraul\Entity\FeedbackEntity $entity */
     $form = parent::buildForm($form, $form_state);
     $form['actions']['submit']['#value'] = $this->t('Send feedback');
+    // Div element to show messages into.
+    $form['system_messages'] = [
+      '#markup' => '<div id="form-system-messages"></div>',
+      '#weight' => -100,
+    ];
+    $form['actions']['submit']['#ajax'] = [
+      'callback' => '::ajaxSubmit',
+      'event' => 'click',
+      'progress' => [
+        'type' => 'throbber',
+      ],
+    ];
     return $form;
   }
 
@@ -61,6 +77,34 @@ class FeedbackEntityAddForm extends ContentEntityForm {
         ]));
     }
     $form_state->setRedirect('yuraul.feedback');
+  }
+
+  public function ajaxSubmit(array &$form, FormStateInterface $form_state) {
+    $ajax_response = new AjaxResponse();
+    // If there are no validation errors sending response with redirect
+    // to feedback page.
+    if (!$form_state->hasAnyErrors()) {
+      $url = Url::fromRoute('yuraul.feedback')->toString();
+      $ajax_response->addCommand(new RedirectCommand($url));
+    }
+    // Else sending response with rendered errors to show it in form.
+    else {
+      $message = [
+        '#theme' => 'status_messages',
+        '#message_list' => \Drupal::messenger()->all(),
+        '#status_headings' => [
+          'status' => t('Status message'),
+          'error' => t('Error message'),
+          'warning' => t('Warning message'),
+        ],
+        '#marckup' => time(),
+      ];
+      \Drupal::messenger()->deleteAll();
+      $messages = \Drupal::service('renderer')->render($message);
+      $ajax_response->addCommand(new HtmlCommand('#form-system-messages', $messages));
+    }
+
+    return $ajax_response;
   }
 
 }
